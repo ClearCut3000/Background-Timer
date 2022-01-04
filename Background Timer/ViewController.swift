100b91fce516809b73f1149a63afa0aa84224961
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-01-05 01:38:32 +0300

    Added actions. Updated functions. Updated comments. Added icon & screenshots

--- Background Timer/ViewController.swift
+++ Background Timer/ViewController.swift
@@ -27,38 +27,70 @@ class ViewController: UIViewController {
   //MARK: - View lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
+    //    Update time form userDefaults if app was closed
     startTime = userDefaults.object(forKey: START_TIME_KEY) as? Date
     stopTime = userDefaults.object(forKey: STOP_TIME_KEY) as? Date
-    timerIsCounting = userDefaults.bool(forKey: COUNTING_KEY )
+    timerIsCounting = userDefaults.bool(forKey: COUNTING_KEY)
+
+    if timerIsCounting {
+      startTimer()
+    } else {
+      stopTimer()
+      if let start = startTime {
+        if let stop = stopTime {
+          let time = calcRestartTime(start: start, stop: stop)
+          let difference = Date().timeIntervalSince(time)
+          setTimeLabel(Int(difference))
+        }
+      }
+    }
   }
 
   //MARK: - Methods
-  func setStartTime(date: Date?){
+
+  /// Updates userDefaults for stopTime
+  /// - Parameter date: startTime date
+  func setStartTime(date: Date?) {
     startTime = date
     userDefaults.set(startTime, forKey: START_TIME_KEY)
   }
 
-  func setStopTime(date: Date?){
+  /// Updates userDefaults for stopTime
+  /// - Parameter date: stopTime date
+  func setStopTime(date: Date?) {
     stopTime = date
     userDefaults.set(stopTime, forKey: STOP_TIME_KEY)
   }
 
-  func setTimerCountiong(_ value: Bool){
+  /// Updates userDefaults for timerIsCounting
+  /// - Parameter value: bool
+  func setTimerCounting(_ value: Bool) {
     timerIsCounting = value
-    userDefaults.set(timerIsCounting, forKey: STOP_TIME_KEY)
+    userDefaults.set(timerIsCounting, forKey: COUNTING_KEY)
   }
 
-  @objc func refreshValue(){
+  /// if there is a value inside startTime, it calculates the difference in milliseconds. If the startTime value is not set, it resets the timer and updates the label via the functions
+  @objc func refreshValue() {
     if let start = startTime {
       let difference = Date().timeIntervalSince(start)
       setTimeLabel(Int(difference))
+    } else {
+      stopTimer()
+      setTimeLabel(0)
     }
   }
 
-  func setTimeLabel(_ value: Int){
-let 
+  /// updates/setups the label
+  /// - Parameter value: integer value for the label
+  func setTimeLabel(_ value: Int) {
+    let time = secToHoursMinSec(value)
+    let timeString = makeTimeString(hour: time.0, min: time.1, sec: time.2)
+    timeLabel.text = timeString
   }
 
+  /// calculates minutes hours seconds from a time interval in milliseconds
+  /// - Parameter ms: time interval - difference in milliseconds
+  /// - Returns: hours minutes seconds as integers
   func secToHoursMinSec(_ ms: Int) -> (Int, Int, Int) {
     let hour = ms / 3600
     let min = (ms % 3600) / 60
@@ -66,8 +98,14 @@ let
     return (hour, min, sec)
   }
 
+  /// Makes a string for the label
+  /// - Parameters:
+  ///   - hour: hour
+  ///   - min: minutes
+  ///   - sec: seconds
+  /// - Returns: label string
   func makeTimeString(hour: Int, min: Int, sec: Int) -> String {
-var timeString = ""
+    var timeString = ""
     timeString += String(format: "%02d", hour)
     timeString += ":"
     timeString += String(format: "%02d", min)
@@ -76,30 +114,54 @@ var timeString = ""
     return timeString
   }
 
-  func stopTimer(){
+  /// Stops the timer, changes the Boolean value to false and changes the title and color of the label
+  func stopTimer() {
     if scheduledTimer != nil { scheduledTimer.invalidate() }
-    setTimerCountiong(false)
+    setTimerCounting(false)
     startStopButton.setTitle("START", for: .normal)
-    startStopButton.setTitleColor(UIColor.green, for: .normal)
+    startStopButton.setTitleColor(UIColor.systemGreen, for: .normal)
   }
 
-  func startTimer(){
+  /// Calculate difference betweeen star time and time of stop
+  /// - Parameters:
+  ///   - start: start time
+  ///   - stop: stop time
+  /// - Returns: difference in the form of a time interval
+  func calcRestartTime(start: Date, stop: Date) -> Date {
+    let difference = start.timeIntervalSince(stop)
+    return Date().addingTimeInterval(difference)
+  }
+
+  /// Starts timer counting and turns boolean value to true
+  func startTimer() {
     scheduledTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(refreshValue), userInfo: nil, repeats: true)
-    setTimerCountiong(true)
+    setTimerCounting(true)
     startStopButton.setTitle("STOP", for: .normal)
     startStopButton.setTitleColor(UIColor.red, for: .normal)
   }
+
   //MARK: - Actions
   @IBAction func startStopAction(_ sender: Any) {
     if timerIsCounting {
       setStopTime(date: Date())
       stopTimer()
     } else {
+      if let stop = stopTime {
+        let restartTime = calcRestartTime(start: startTime!, stop: stop)
+        setStopTime(date: nil)
+        setStartTime(date: restartTime)
+      } else {
+        setStartTime(date: Date())
+      }
       startTimer()
     }
   }
 
   @IBAction func resetAction(_ sender: Any) {
+    setStopTime(date: nil)
+    setStartTime(date: nil)
+    timeLabel.text = makeTimeString(hour: 0, min: 0, sec: 0)
+    stopTimer()
   }
 }
 
